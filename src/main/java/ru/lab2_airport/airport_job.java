38f3a20a77a4098ef4b19b0adce9b36133846638
OsosovPavel <Ososowpp@yandex.ru>
2020-10-23 21:57:42 +0300

    Scripted auto-commit on change (2020-10-23 21:57:42) by gitwatch.sh

--- src/main/java/ru/lab2_airport/airport_job.java
+++ src/main/java/ru/lab2_airport/airport_job.java
@@ -22,8 +22,7 @@ public class airport_job {
       job.setJarByClass(FlightJob.class);
       job.setJobName("JoinJob");
       MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
-
+      MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, NametMapper.class);
     }
 
 
