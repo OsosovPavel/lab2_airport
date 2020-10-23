dbca86a7adf139513c3493ed9137c5956d03c70f
OsosovPavel <Ososowpp@yandex.ru>
2020-10-23 21:48:54 +0300

    Scripted auto-commit on change (2020-10-23 21:48:54) by gitwatch.sh

--- src/main/java/ru/lab2_airport/airport_job.java
+++ src/main/java/ru/lab2_airport/airport_job.java
@@ -2,6 +2,8 @@ package ru.lab2_airport;
 
 import org.apache.hadoop.fs.Path;
 
+i
+
 public class airport_job {
     public static void main(String[] args) throws Exception{
         if(args.length != 3){
