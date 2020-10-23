384c105a5fa65bb0243f4e1f99035f25e978fca3
OsosovPavel <Ososowpp@yandex.ru>
2020-10-23 21:51:09 +0300

    Scripted auto-commit on change (2020-10-23 21:51:09) by gitwatch.sh

--- src/main/java/ru/lab2_airport/airport_job.java
+++ src/main/java/ru/lab2_airport/airport_job.java
@@ -3,6 +3,14 @@ package ru.lab2_airport;
 import org.apache.hadoop.fs.Path;
 
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+
+
+
+
+
+
+
 
 public class airport_job {
     public static void main(String[] args) throws Exception{
@@ -10,7 +18,7 @@ public class airport_job {
             System.exit(-1);
         }
       Job job =Job.getIntances();
-      job.setJarByClass(FlightJob.class)  
+      job.setJarByClass(FlightJob.class)
 
     }
 
