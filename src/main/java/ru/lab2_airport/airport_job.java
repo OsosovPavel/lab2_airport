2a645d8873691150feb88f167f8438387d710ff9
OsosovPavel <Ososowpp@yandex.ru>
2020-10-23 21:48:34 +0300

    Scripted auto-commit on change (2020-10-23 21:48:34) by gitwatch.sh

--- src/main/java/ru/lab2_airport/airport_job.java
+++ src/main/java/ru/lab2_airport/airport_job.java
@@ -3,7 +3,13 @@ package ru.lab2_airport;
 import org.apache.hadoop.fs.Path;
 
 public class airport_job {
-    public static void main(String[] args) throws
+    public static void main(String[] args) throws Exception{
+        if(args.length != 3){
+            System.exit(-1);
+        }
+      Job job =Job.getIntances();
+
+    }
 
 
 
