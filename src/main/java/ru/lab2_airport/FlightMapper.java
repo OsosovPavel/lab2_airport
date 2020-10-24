eaced3dbdd0c836273e601e7c36d41152c41e847
OsosovPavel <Ososowpp@yandex.ru>
2020-10-24 13:33:31 +0300

    Scripted auto-commit on change (2020-10-24 13:33:31) by gitwatch.sh

--- src/main/java/ru/lab2_airport/FlightMapper.java
+++ src/main/java/ru/lab2_airport/FlightMapper.java
@@ -1,15 +1,19 @@
 package ru.lab2_airport;
 
 import org.appache.hadoop.io.IntWritable;
+import org.appache.hadoop.io.LongWritable;
+import org.appache.hadoop.io.Text;
 import org.appache.hadoop.io.IntWritable;
-import org.appache.hadoop.io.IntWritable;
-import org.appache.hadoop.io.IntWritable;
 
 
 
 
 
 
-public class FlightMapper extends Mapper<LongWritable, Text, WComparable> {
+public class FlightMapper extends Mapper<LongWritable, Text, WComparable, Text> {
+
+    private static String SEPARATOR = ",";
+
+
 
 }
