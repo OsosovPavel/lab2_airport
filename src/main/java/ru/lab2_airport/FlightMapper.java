ebb0bf48569c80e90ea6eed8dc2b572d9e29f268
OsosovPavel <Ososowpp@yandex.ru>
2020-10-24 14:20:20 +0300

    Scripted auto-commit on change (2020-10-24 14:20:20) by gitwatch.sh

--- src/main/java/ru/lab2_airport/FlightMapper.java
+++ src/main/java/ru/lab2_airport/FlightMapper.java
@@ -8,21 +8,27 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 
-
-
-
-
 public class FlightMapper extends Mapper<LongWritable, Text, WComparable, Text> {
 
     private static String SEPARATOR = ",";
-    private static int DEST_AIRPORT_ID = 14;
+    private static int DEST_AIRPORT_ID_LEN = 14;
+    private static int ARR_DELAY_POSITION = 17;
 
 @Override
 protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-    if(key.get() !=0){
+    if (key.get() != 0) {
         String[] table = value.toString().split(SEPARATOR);
+        int airportID = Integer.parseInt(table[DEST_AIRPORT_ID_LEN]);
+        double airportDelay = getDelay(table[ARR_DELAY_POSITION]);
+        if (airportDelay > 0) {
+            WComparable comparable = new WComparable(airportID, 1);
+            context.write(comparable, new Text(Double.toString(airportDelay)));
+        }
     }
 }
 
+private double getDelay(String value){
+    return value.equals("") ? 0.0 : Double.parseDouble(value);
+}
 
 }
--- src/main/java/ru/lab2_airport/airport_job.java
+++ src/main/java/ru/lab2_airport/airport_job.java
@@ -12,7 +12,7 @@ public class airport_job {
             System.exit(-1);
         }
       Job job =Job.getInstance();
-      job.setJarByClass(FlightJob.class);
+      job.setJarByClass(airport_job.class);
       job.setJobName("JoinJob");
       MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
       MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, NametMapper.class);
