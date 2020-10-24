2415a9fb0b164813b65dcd52a7bbd31f1f360398
OsosovPavel <Ososowpp@yandex.ru>
2020-10-24 13:57:23 +0300

    Scripted auto-commit on change (2020-10-24 13:57:23) by gitwatch.sh

--- src/main/java/ru/lab2_airport/FlightMapper.java
+++ src/main/java/ru/lab2_airport/FlightMapper.java
@@ -3,7 +3,9 @@ package ru.lab2_airport;
 import org.appache.hadoop.io.IntWritable;
 import org.appache.hadoop.io.LongWritable;
 import org.appache.hadoop.io.Text;
-import org.appache.hadoop.io.IntWritable;
+import org.appache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 
 
 
@@ -13,7 +15,14 @@ import org.appache.hadoop.io.IntWritable;
 public class FlightMapper extends Mapper<LongWritable, Text, WComparable, Text> {
 
     private static String SEPARATOR = ",";
+    private static int DEST_AIRPORT_ID = 14;
 
+@Override
+protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    if(key.get() !=0){
+        String[] table = value.toString().split(SEPARATOR);
+    }
+}
 
 
 }
