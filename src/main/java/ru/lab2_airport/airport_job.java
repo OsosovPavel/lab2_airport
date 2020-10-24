ce51912aa7deb2b22d9c239dd72a6abd564f9a8d
OsosovPavel <Ososowpp@yandex.ru>
2020-10-24 13:59:10 +0300

    Scripted auto-commit on change (2020-10-24 13:59:10) by gitwatch.sh

--- src/main/java/ru/lab2_airport/airport_job.java
+++ src/main/java/ru/lab2_airport/airport_job.java
@@ -1,16 +1,9 @@
 package ru.lab2_airport;
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-
-
-
-
 
 
 public class airport_job {
