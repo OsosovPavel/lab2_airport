c50a03b39c4ac91406ce07ebb0d769ece0bfa2d6
OsosovPavel <Ososowpp@yandex.ru>
2020-10-24 15:31:16 +0300

    Scripted auto-commit on change (2020-10-24 15:31:16) by gitwatch.sh

--- src/main/java/ru/lab2_airport/GComparator.java
+++ src/main/java/ru/lab2_airport/GComparator.java
@@ -1,4 +1,21 @@
 package ru.lab2_airport;
 
-public class GComparator {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableComparable;
+
+public class GComparator extends WritableComparable {
+    protected GComparator(){
+        super(WComparable.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable a, WritableComparable b){
+        WComparable wc1 = (WComparable) a;
+        WComparable wc2 = (WComparable) b;
+        return wc1.compare(wc2);
+    }
 }
